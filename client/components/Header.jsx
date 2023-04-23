3c0cb6eb49d8aab25f37d05bb1c5f5a91f05b0b1
Akshatjain <akshatj@cabin4j.com>
2023-04-23 23:12:08 +0530

    showstore

---
+++ client/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import Wrapper from './Wrapper'
+import Link from 'next/link'
+import Menu from './Menu'
+
+const Header = () => {
+    const [mobileMenu,setMobileMenu]=useState(false)
+    const [showCatMenu,setShowCatMenu]=useState(false)
+    const [show,setShow]=useState("transalate-y-0");
+    const [lastScrollY,setLastScrollY]=useState(0)
+  return (
+    <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300${show}`}>
+        <Wrapper>
+            <Link href="/">
+            <img src='./logo.svg' className='w-[40px] md:w-[60px]'/>
+            </Link>
+            <Menu/>
+        </Wrapper>
+    </header>
+  )
+}
+
+export default Header
---
+++ client/components/Menu.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Link from 'next/link';
+import {BsChevronDown} from "react-icons/bs"
+const data = [
+    { id: 1, name: "Home", url: "/" },
+    { id: 2, name: "About", url: "/about" },
+    { id: 3, name: "Categories", subMenu: true },
+    { id: 4, name: "Contact", url: "/contact" },
+];
+
+const subMenuData = [
+    { id: 1, name: "Jordan", doc_count: 11 },
+    { id: 2, name: "Sneakers", doc_count: 8 },
+    { id: 3, name: "Running shoes", doc_count: 64 },
+    { id: 4, name: "Football shoes", doc_count: 107 },
+];
+
+const Menu = () => {
+  return (
+    <ul className='hidden md:flex items-center gap-8 font-medium text-black'>
+    {
+        data.map((item)=>{
+            return (
+                <>
+                 <React.Fragment key={item.id}>
+                    {
+                        !!item?.subMenu ? "Submenu" :
+                        (
+                            <>
+                            <Link href={item?.url}>
+                            <li className='cursor-pointer'>{item.name}</li>
+                            </Link>
+                            </>
+                        ) 
+                    }
+                 </React.Fragment>
+                </>
+            )
+        })
+    }
+    </ul>
+  )
+}
+
+export default Menu
