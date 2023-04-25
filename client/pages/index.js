b6fc7e645d3036c815d0133eb9538c3737175fcc
Akshatjain <akshat.2024it1031@kiet.edu>
2023-04-25 15:23:52 +0530

    added footer and herobbanner

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,20 @@
-import Head from 'next/head'
-
-
+import Head from "next/head";
+import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
   return (
     <>
-    <main>Homepage</main>
+      <main>
+        <HeroBanner/>
+        <Wrapper>
+          <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+            <div>Heading</div>
+            <div>Paragraph</div>
+          </div>
+        </Wrapper>
+        
+      </main>
     </>
-  )
+  );
 }
