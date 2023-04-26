b6d4c5b09f930de4d43eb633d2984af133920ba6
Akshatjain <akshat.2024it1031@kiet.edu>
2023-04-26 12:24:39 +0530

    added crousel

--- client/pages/product/[slug].jsx
+++ client/pages/product/[slug].jsx
@@ -1,16 +1,19 @@
 import Wrapper from "@/components/Wrapper";
 import React from "react";
+import ProductCrousel  from "@/components/ProductCrousel";
 
 const ProductDetails = () => {
   return (
     <div className="w-full md:py-20">
       <Wrapper>
+        <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
         {/* Left coloumn starts */}
-        <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lf:mx-0">
-          Left
+        <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
+          <ProductCrousel/>
         </div>
         {/* Right column starts */}
         <div className="flex-[1] py-3">Right</div>
+        </div>
       </Wrapper>
     </div>
   );
