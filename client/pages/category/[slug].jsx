05af40c265788ece5f330414e50b573c7cde691f
Akshatjain <akshat.2024it1031@kiet.edu>
2023-05-03 19:37:35 +0530

    added dynamics routes

--- client/pages/category/[slug].jsx
+++ client/pages/category/[slug].jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Wrapper from "@/components/Wrapper";
 import ProductCard from "@/components/ProductCard";
+import { fetchDataFromApi } from "@/utils/api";
 
-const Category = () => {
+const Category = ({ category, products, slugs }) => {
   return (
     <div className="w-full md:py-20">
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-light">
-            Running Shoes
+            {category?.data?.[0]?.attributes?.name}
           </div>
         </div>
         {/* Heading and caption section ends */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-6">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))}
+          {/* <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard /> */}
         </div>
       </Wrapper>
     </div>
@@ -26,3 +30,33 @@ const Category = () => {
 };
 
 export default Category;
+
+export async function getStaticPaths() {
+  const category = await fetchDataFromApi("/api/categories?populate=*");
+  const paths = category.data.map((c) => ({
+    params: {
+      slug: c.attributes.slug,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const category = await fetchDataFromApi(
+    `/api/categories?filters[slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}`
+  );
+
+  return {
+    props: {
+      category,
+      products,
+      slug,
+    },
+  };
+}
