7ce73bbcb473f53165fb4901c36c08d02300e709
Akshatjain <akshat.2024it1031@kiet.edu>
2023-05-16 19:22:44 +0530

    corrected

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import Link from 'next/link';
-import {BsChevronDown} from "react-icons/bs"
+import React from "react";
+import Link from "next/link";
+import { BsChevronDown } from "react-icons/bs";
+
 const data = [
     { id: 1, name: "Home", url: "/" },
     { id: 2, name: "About", url: "/about" },
@@ -15,57 +16,58 @@ const subMenuData = [
     { id: 4, name: "Football shoes", doc_count: 107 },
 ];
 
-const Menu = ({showCatMenu,setShowCatMenu,categories}) => {
-  return (
-    <ul className='hidden md:flex items-center gap-8 font-medium text-black'>
-    {
-        data.map((item)=>{
-            return (
-                <>
-                 <React.Fragment key={item.id}>
-                    {
-                        !!item?.subMenu ? (<>
-                        <li className="cursor-pointer flex items-center gap-2 relative"
-                        onMouseEnter={()=>setShowCatMenu(true)}
-                        onMouseLeave={()=>setShowCatMenu(false)}>
-                            {item.name}
-                            <BsChevronDown size={14}/>
-                            {showCatMenu && (
-                                <>
-                                <ul className='bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1 text-black shadow-lg'>
-                                {categories?.map(({attributes:c,id})=>{
-                                    return (
-                                        <Link key={id} 
-                                        href={`/category/${c.slug}`}
-                                        onClick={()=>setShowCatMenu(false)}>
-                                            <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
-                                                {c.name}
-                                                <span className='opacity-50 text-sm'>{`(${c.products.data.length})`}</span>
-                                            </li>
+const Menu = ({ showCatMenu, setShowCatMenu, categories }) => {
+    return (
+        <ul className="hidden md:flex items-center gap-8 font-medium text-black">
+            {data.map((item) => {
+                return (
+                    <React.Fragment key={item.id}>
+                        {!!item?.subMenu ? (
+                            <li
+                                className="cursor-pointer flex items-center gap-2 relative"
+                                onMouseEnter={() => setShowCatMenu(true)}
+                                onMouseLeave={() => setShowCatMenu(false)}
+                            >
+                                {item.name}
+                                <BsChevronDown size={14} />
 
-                                        </Link>
-                                    )
-                                })}
-                                </ul>
-                                </>
-                            )}
-                        </li>
-                        </>) :
-                        (
-                            <>
-                            <Link href={item?.url}>
-                            <li className='cursor-pointer'>{item.name}</li>
-                            </Link>
-                            </>
-                        ) 
-                    }
-                 </React.Fragment>
-                </>
-            )
-        })
-    }
-    </ul>
-  )
-}
+                                {showCatMenu && (
+                                    <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1 text-black shadow-lg">
+                                        {categories?.map(
+                                            ({ attributes: c, id }) => {
+                                                return (
+                                                    <Link
+                                                        key={id}
+                                                        href={`/category/${c.slug}`}
+                                                        onClick={() =>
+                                                            setShowCatMenu(
+                                                                false
+                                                            )
+                                                        }
+                                                    >
+                                                        <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
+                                                            {c.name}
+                                                            <span className="opacity-50 text-sm">
+                                                                {`(${c.products.data.length})`}
+                                                            </span>
+                                                        </li>
+                                                    </Link>
+                                                );
+                                            }
+                                        )}
+                                    </ul>
+                                )}
+                            </li>
+                        ) : (
+                            <li className="cursor-pointer">
+                                <Link href={item?.url}>{item.name}</Link>
+                            </li>
+                        )}
+                    </React.Fragment>
+                );
+            })}
+        </ul>
+    );
+};
 
-export default Menu
+export default Menu;
