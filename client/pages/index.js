5d71199ea62fc020915417fcc317fd995c45ebfa
Akshatjain <akshat.2024it1031@kiet.edu>
2023-05-16 19:31:39 +0530

    minor changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ products }) {
           {/* <h1>{products?.data?.[0]?.attributes?.name}</h1> */}
           <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
             <div className="text-[28px] md:text-[34px] mb-5 font-semibold">
-              Heading
+              Dashing Products
             </div>
             <div className="text-md md:text-xl">
               A lightweight Niko ZoomX midsole is combined with increased stack
