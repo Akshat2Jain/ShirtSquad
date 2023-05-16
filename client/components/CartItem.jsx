2c7c73771c5aabebf99428f82e63bf5c99ab6548
Akshatjain <akshat.2024it1031@kiet.edu>
2023-05-16 18:10:54 +0530

    cart funtionality completed

--- client/components/CartItem.jsx
+++ client/components/CartItem.jsx
@@ -1,115 +1,119 @@
 import Image from "next/image";
 import React from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { updateCart } from "@/store/cartSlice";
+import { updateCart, removeFromCart } from "@/store/cartSlice";
 import { useDispatch } from "react-redux";
-
 const CartItem = ({ data }) => {
-  const p = data.attributes;
-  const dispatch = useDispatch();
+    const p = data.attributes;
 
-  const updateCartItem = (e, key) => {
-    let payload = {
-      key,
-      val: key === "quantity" ? parseInt(e.target.value) : e.target.value,
-      id: data.id,
+    const dispatch = useDispatch();
+
+    const updateCartItem = (e, key) => {
+        let payload = {
+            key,
+            val: key === "quantity" ? parseInt(e.target.value) : e.target.value,
+            id: data.id,
+        };
+        dispatch(updateCart(payload));
     };
-    dispatch(updateCart(payload));
-  };
 
-  return (
-    <>
-      <div className="flex py-5 gap-3 md:gap-5 border-b">
-        <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
-          {/* <img src="p1.png" alt="img" /> */}
-          <Image
-            src={p.thumbnail.data.attributes.url}
-            alt={p.name}
-            width={120}
-            height={120}
-          />
+    return (
+        <div className="flex py-5 gap-3 md:gap-5 border-b">
+            {/* IMAGE START */}
+            <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
+                <Image
+                    src={p.thumbnail.data.attributes.url}
+                    alt={p.name}
+                    width={120}
+                    height={120}
+                />
+            </div>
+            {/* IMAGE END */}
+
+            <div className="w-full flex flex-col">
+                <div className="flex flex-col md:flex-row justify-between">
+                    {/* PRODUCT TITLE */}
+                    <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
+                        {p.name}
+                    </div>
+
+                    {/* PRODUCT SUBTITLE */}
+                    <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
+                        {p.subtitle}
+                    </div>
+
+                    {/* PRODUCT PRICE */}
+                    <div className="text-sm md:text-md font-bold text-black/[0.5] mt-2">
+                        MRP : &#8377;{p.price}
+                    </div>
+                </div>
+
+                {/* PRODUCT SUBTITLE */}
+                <div className="text-md font-medium text-black/[0.5] hidden md:block">
+                    {p.subtitle}
+                </div>
+
+                <div className="flex items-center justify-between mt-4">
+                    <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
+                        <div className="flex items-center gap-1">
+                            <div className="font-semibold">Size:</div>
+                            <select
+                                className="hover:text-black"
+                                onChange={(e) =>
+                                    updateCartItem(e, "selectedSize")
+                                }
+                            >
+                                {p.size.data.map((item, i) => {
+                                    return (
+                                        <option
+                                            key={i}
+                                            value={item.size}
+                                            disabled={
+                                                !item.enabled ? true : false
+                                            }
+                                            selected={
+                                                data.selectedSize === item.size
+                                            }
+                                        >
+                                            {item.size}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+
+                        <div className="flex items-center gap-1">
+                            <div className="font-semibold">Quantity:</div>
+                            <select
+                                className="hover:text-black"
+                                onChange={(e) => updateCartItem(e, "quantity")}
+                            >
+                                {Array.from(
+                                    { length: 10 },
+                                    (_, i) => i + 1
+                                ).map((q, i) => {
+                                    return (
+                                        <option
+                                            key={i}
+                                            value={q}
+                                            selected={data.quantity === q}
+                                        >
+                                            {q}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                    </div>
+                    <RiDeleteBin6Line
+                        onClick={() =>
+                            dispatch(removeFromCart({ id: data.id }))
+                        }
+                        className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]"
+                    />
+                </div>
+            </div>
         </div>
-        {/* ***************** */}
-        <div className="w-full flex flex-col">
-          <div className="flex flex-col md:flex-row justify-between">
-            {/* Product title */}
-            <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
-              {p.name}
-            </div>
-            {/* Product Subtitle */}
-            <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
-              {p.subtitle}
-            </div>
-            {/* Product Price */}
-            <div className="text-sm md:text-md font-bold text-black/[0.5] mt-2">
-              MRP : &#8377;{p.price}
-            </div>
-          </div>
-          {/* Product subtitle */}
-          <div className="text-md font-medium text-black/[0.5] hidden md:block">
-            {p.subtitle}
-          </div>
-          {/* Size */}
-          <div className="flex items-center justify-between mt-4">
-            <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
-              <div className="flex items-center gap-1">
-                <div className="font-semibold">Size</div>
-                <select
-                  className="hover:text-black"
-                  onChange={(e) => {
-                    updateCartItem(e, "selectedSize");
-                  }}
-                >
-                  {p.size.data.map((item, i) => {
-                    return (
-                      <option
-                        key={i}
-                        value={item.size}
-                        disabled={!item.enabled ? true : false}
-                        selected={data.selectedSize === item.size}
-                      >
-                        {item.size}
-                      </option>
-                    );
-                  })}
-                  {/* <option value="1">UK6</option>
-                  <option value="1">UK6</option>
-                  <option value="1">UK6</option>
-                  <option value="1">UK6</option>
-                  <option value="1">UK6</option>
-                  <option value="1">UK6</option> */}
-                </select>
-              </div>
-              <div className="flex items-center gap-1">
-                <div className="font-semibold">Quantity</div>
-                <select
-                  className="hover:text-black"
-                  onChange={(e) => {
-                    updateCartItem(e, "selectedSize");
-                  }}
-                >
-                  {Array.from({ length: 10 }, (_, i) => i + 1).map((q, i) => {
-                    return (
-                      <option key={i} value={q} selected={data.quantity === q}>
-                        {q}
-                      </option>
-                    );
-                  })}
-                  {/* <option value="1">1</option>
-                  <option value="1">2</option>
-                  <option value="1">3</option>
-                  <option value="1">4</option>
-                  <option value="1">5</option>
-                  <option value="1">6</option> */}
-                </select>
-              </div>
-            </div>
-            <RiDeleteBin6Line className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]" />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    );
 };
-
 export default CartItem;
