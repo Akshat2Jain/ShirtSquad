b599c0632c7842bf8c876c568f5616b3403916a4
Akshatjain <akshat.2024it1031@kiet.edu>
2023-05-17 18:52:50 +0530

    added profile links

--- client/pages/contact/index.js
+++ client/pages/contact/index.js
@@ -9,19 +9,26 @@ const index = () => {
         <Wrapper>
           <div className="max-w-[600px] rounded-lg p-5 border border-black mx-auto flex flex-col">
             <div className="text-2xl font-bold">Contact the Developer</div>
-            <div className="text-lg font-bold mt-2">
-              Profile Links 👇
-            </div>
+            <div className="text-lg font-bold mt-2">Profile Links 👇</div>
             <div className="text-base mt-5">
               <ul>
-                <Link href="https://github.com/Akshat2Jain" target="_blank"><li>GitHub</li></Link>
-                <li>Twitter</li>
-                <li>Linkedin</li>
-                <li>Gmail</li>
+                <Link href="https://github.com/Akshat2Jain" target="_blank">
+                  <li>GitHub</li>
+                </Link>
+                <Link href="https://twitter.com/Easyakshat" target="_blank">
+                  <li>Twitter</li>
+                </Link>
+                <Link
+                  href="https://www.linkedin.com/in/akshat-jain-991571209/"
+                  target="_blank"
+                >
+                  <li>Linkedin</li>
+                </Link>
+                <Link href="mailto:jainakshat423@gmail.com" target="_blank">
+                  <li>Gmail</li>
+                </Link>
               </ul>
             </div>
-
-          
 
             <Link href="/" className="font-bold mt-5">
               Continue Shopping
