edfaf0943eaacca7c470ad03ca984367b88b0e58
Akshatjain <akshat.2024it1031@kiet.edu>
2023-05-17 18:27:46 +0530

    added minor changes

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -71,12 +71,12 @@ const Header = () => {
         )}
         <div className="flex items-center gap-2 text-black">
           {/* 1st Icon */}
-          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+          {/* <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
             <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
             <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
               51
             </div>
-          </div>
+          </div> */}
           {/* 2nd Icon */}
           <Link href="/cart">
             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
--- client/pages/product/[slug].jsx
+++ client/pages/product/[slug].jsx
@@ -128,10 +128,10 @@ const ProductDetails = ({ product, products }) => {
                 Add to Cart
               </button>
               {/* Add to wishlist button */}
-              <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
+              {/* <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
                 Whishlist
                 <IoMdHeartEmpty size={20} />
-              </button>
+              </button> */}
               {/* Product Desctription */}
               <div>
                 <div className="text-lg font-bold mb-5">Product Details</div>
