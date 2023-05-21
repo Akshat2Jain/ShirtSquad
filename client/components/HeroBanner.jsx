d2e3cb24cd6802a62ef8e7f0bf1458cdf5013fe7
Akshatjain <akshat.2024it1031@kiet.edu>
2023-05-21 13:09:24 +0530

    added new banner

--- client/components/HeroBanner.jsx
+++ client/components/HeroBanner.jsx
@@ -53,7 +53,7 @@ const HeroBanner = () => {
 
         <div>
           <img
-            src="/slide-3.png"
+            src="/banner3.png"
             className="aspect-[16/10] md:aspect-auto object-cover"
           />
           <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
